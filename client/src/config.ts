58bbf342b5c3490a73130add5b98258b75f9028c
MohanKrishna <www.krishna225@gmail.com>
2020-04-07 02:56:47 +0530

    TODO  project

---
+++ client/src/config.ts
@@ -0,0 +1,10 @@
+
+const apiId = 'zm4pwpef06'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  
+  domain: 'mohan225.auth0.com',            // Auth0 domain
+  clientId: 'evR2xXCm0KAcS6asoW5NRbOicz7Uc9Ee',          // Auth0 client id
+  callbackUrl: 'http://localhost:3000/callback'
+}
